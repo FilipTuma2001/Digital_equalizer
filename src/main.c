5fa4a80c806879ea708ae9483939d727ed6e3f34
Filda PC <Filipek PC>
2024-05-18 23:24:07 +0200

    adding comments

--- src/main.c
+++ src/main.c
@@ -1,14 +1,7 @@
-/***********************************************************************
- *
- * Use USART unit and transmit data between ATmega328P and computer.
- *
- * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
- *
- * Copyright (c) 2018 Tomas Fryza
- * Dept. of Radio Electronics, Brno University of Technology, Czechia
- * This work is licensed under the terms of the MIT license.
- *
- **********************************************************************/
+/* 
+Bachelor´s work - 7 Band Digital Equalizer
+Filip Tuma (2024)
+*/
 
 /* Defines -----------------------------------------------------------*/
 #ifndef F_CPU
@@ -27,25 +20,32 @@
 #include <twi.h>           // I2C/TWI library for AVR-GCC
 #include <gpio.h>          // GPIO library for AVR-GCC
 #include <util/delay.h>    // Functions for busy-wait delay loops
+#include <avr/pgmspace.h>
+
+// Sampling Rate 44.1 kHz
 //#include "equalizer_registers.h"
 //#include "pp_1kz.h"
 //#include "transport.h"
-#include <avr/pgmspace.h>
 //#include "agc.h"
+
+// Sampling Rate 48 kHz
 //#include "equalizer_pop.h"
 //#include "equalizer_rap.h"
 //#include "equalizer_rock.h"
 #include "vstup_vystup.h"
 
+// Hexadecimal I2C Address of TLV320AIC3254
 #define SENSOR_ADR 0x18
 
+// Function for sending register via I2C
 void equ_write_full(reg_value const *reg_values, size_t size)
 {
     /*
+    // Reading of register (testing) 
     reg_values++;
     char str[8]; // str for converted numbers by itoa()
     itoa(pgm_read_byte_near(&reg_values->reg_off), str, 10);
-    uart_puts("Co to bude?:");
+    uart_puts("Testing:");
     uart_puts(str);
     uart_puts("\r\n");
     */
@@ -54,42 +54,33 @@ void equ_write_full(reg_value const *reg_values, size_t size)
     {
         twi_start();
         twi_write((SENSOR_ADR<<1) | TWI_WRITE);
-                
+
+        // Write into defined register        
         twi_write(pgm_read_byte_near(&reg_values->reg_off));
+        // Write into register defined value
         twi_write(pgm_read_byte_near(&reg_values->reg_val));
         
         twi_stop();
         reg_values++;
     }
 }
-/* Function definitions ----------------------------------------------*/
-/**********************************************************************
- * Function: Main function where the program execution begins
- * Purpose:  Use Timer/Counter1 and transmit UART data four times
- *           per second.
- * Returns:  none
- **********************************************************************/
+
 int main(void)
 {
 
-    // Initialize USART to asynchronous, 8N1, 9600
+    // Initialize USART to asynchronous
     uart_init(UART_BAUD_SELECT(9600, F_CPU));
-
-    // Configure 16-bit Timer/Counter1 to transmit UART data
-    // Set prescaler to 262 ms and enable overflow interrupt
-    // TIM1_OVF_262MS;
-    // TIM1_OVF_ENABLE;
 
     // Enables interrupts by setting the global interrupt mask
     sei();
 
-    // Put strings to ringbuffer for transmitting via UART
-
+    // Set low pin D8 on Arduino UNO (RESET of TLV320AIC3254)
     GPIO_mode_output(&DDRB, 0);
     GPIO_write_low(&PORTB, 0);
     _delay_ms(1);
     GPIO_write_high(&PORTB, 0);
 
+    // Control of connection 
     uart_puts("Starting... ");
     uart_puts("Is AIC3254 connected?\r\n");
 
@@ -101,7 +92,7 @@ int main(void)
     {
         uart_puts("No, device not found\r\n");
     }
-
+/*
     twi_start();
     twi_write((SENSOR_ADR << 1) | TWI_WRITE);
     twi_write(0x00);
@@ -124,7 +115,7 @@ int main(void)
     uart_puts("First reg :");
     uart_puts(str2);
     uart_puts("\r\n");
-    /*
+    
     twi_start();
     twi_write((SENSOR_ADR << 1) | TWI_WRITE);
     twi_write(0x05);
@@ -145,15 +136,16 @@ int main(void)
     uart_puts("another test:");
     uart_puts(str);
     uart_puts("\r\n");
-    */
+*/
 
+    // Send registers into TLV320AIC3254
     equ_write_full(REG_Section_program, sizeof(REG_Section_program) / sizeof(REG_Section_program[0]));
     equ_write_full(miniDSP_A_reg_values, miniDSP_A_reg_values_COEFF_SIZE + miniDSP_A_reg_values_INST_SIZE);
     equ_write_full(miniDSP_D_reg_values, miniDSP_D_reg_values_COEFF_SIZE + miniDSP_D_reg_values_INST_SIZE);
 
     uart_puts("Upload of registers is done!\r\n");
     
-
+/*
     twi_start();
     twi_write((SENSOR_ADR << 1) | TWI_WRITE);
     twi_write(0x00);
@@ -176,43 +168,12 @@ int main(void)
     uart_puts("Page reg (161?):");
     uart_puts(str);
     uart_puts("\r\n");
- 
+*/
     // Infinite loop
     while (1)
-    {
-        /* Empty loop. All subsequent operations are performed exclusively
-         * inside interrupt service routines ISRs */
+    {   
     }
 
     // Will never reach this
     return 0;
-}
-
-/* Interrupt service routines ----------------------------------------*/
-/**********************************************************************
- * Function: Timer/Counter1 overflow interrupt
- * Purpose:  Transmit UART data four times per second.
- **********************************************************************/
-ISR(TIMER1_OVF_vect)
-{
-
-    uint8_t value;
-    char str[8]; // str for converted numbers by itoa()
-
-    value = uart_getc();
-    if (value != '\0')
-    { // Data available from UART
-        // Display ASCII code of received character
-        // WRITE YOUR CODE HERE
-        itoa(value, str, 10);
-        uart_puts("\x1b[4;32m"); // 4: underline style; 32: green foreground
-        uart_puts(str);
-        uart_puts("\r\n");
-        itoa(value, str, 2);
-        uart_puts(str);
-        uart_puts("\r\n");
-        itoa(value, str, 16);
-        uart_puts(str);
-        uart_puts("\r\n");
-    }
 }
